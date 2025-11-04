1368a6c2bc37efb4659c768c0f97b3c45453c25b
mmscichu <przemyslaw.kwasniewski02@student.wat.edu.pl>
2025-11-04 14:34:53 +0100

    Czwarty commit zmodyfikowano main.cpp (std::flush)

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main(){
 
         cout << "Witamy w swiecie GIt\n";
-        cout << "Laboratorium nr 1" << endl;
+        cout << "Laboratorium nr 1" << std::flush << endl;
         return 0;
 }
